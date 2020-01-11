ce216276d6de7fd79e9332d483e66de8d189a56a
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2020-01-11 16:53:54 -0300

    Create add contact button

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -7,28 +7,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper contactHelper = ContactHelper();
 
+  List<Contact> contacts = List();
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
 
-    Contact contact = Contact();
-    contact.name = "Lukinhas Rodrigues";
-    contact.email = "lukinhas@gmail.com";
-    contact.phone = "99602-5678";
-    contact.img = "teste/de/imagem2";
-
-    contactHelper.saveContact(contact);
-
     contactHelper.getAllContacts().then((list) {
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, item) {
+
+        },
+      ),
+    );
   }
 }
