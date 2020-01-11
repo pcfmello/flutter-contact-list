8505a2375c91a89676407fb4b9e72ddb81400001
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2020-01-11 17:23:59 -0300

    Create contact list and its card to show

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contact_list/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -40,10 +42,49 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         padding: EdgeInsets.all(8.0),
         itemCount: contacts.length,
-        itemBuilder: (context, item) {
-
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
         },
       ),
     );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+        child: Card(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: contacts[index].img != null
+                            ? FileImage(File(contacts[index].img))
+                            : AssetImage("images/person.png")))),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(contacts[index].name ?? "",
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  Text(contacts[index].name ?? "",
+                      style: TextStyle(
+                        fontSize: 18,
+                      )),
+                  Text(contacts[index].name ?? "",
+                      style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
